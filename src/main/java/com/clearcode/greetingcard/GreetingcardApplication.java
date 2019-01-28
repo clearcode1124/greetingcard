2ef9cd36e9d3607b4e8210eeceaf0900fa927498
wdChen <wdChen@hua9group.com>
2019-01-28 15:38:49 +0800

    init

---
+++ src/main/java/com/clearcode/greetingcard/GreetingcardApplication.java
@@ -0,0 +1,14 @@
+package com.clearcode.greetingcard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreetingcardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreetingcardApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/clearcode/greetingcard/greetingcard/GreetingcardApplicationTests.java
@@ -0,0 +1,17 @@
+package com.clearcode.greetingcard.greetingcard;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GreetingcardApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
